8df591d38d10bdb8d0b8274d755739decf31ac7b
narensendil18 <narensendil18@vt.edu>
2026-04-12 13:49:58 -0400

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findDuplicate(int[] nums) 
+    {
+        int slow = nums[0];
+        int fast = nums[nums[0]]; 
+
+        // phase 1: find intersection
+        while (slow != fast)
+        {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+
+        // phase 2: find entrance
+        int ptr = 0; 
+        while (ptr != slow)
+        {
+            ptr = nums[ptr];
+            slow = nums[slow];
+        }
+
+        return ptr;
+    }
+}
